f8c15a6a837befc85a8f5cdb17a375944300d3dd
pk1088141-alt <pk1088141@gmail.com>
2026-07-24 09:59:26 +0530

    Update news.js

--- api/news.js
+++ api/news.js
@@ -1,47 +1,18 @@
 export default async function handler(req, res) {
+  const API_KEY = process.env.NEWSDATA_API_KEY;
 
-const API_KEY = process.env.NEWSDATA_API_KEY;
+  try {
+    const response = await fetch(
+      `https://newsdata.io/api/1/latest?apikey=${API_KEY}&country=in&language=en`
+    );
 
-const {
-page = "",
-q = "",
-category = ""
-} = req.query;
+    const data = await response.json();
 
-let url =
-`https://newsdata.io/api/1/latest?apikey=${API_KEY}&language=en`;
-
-if(q){
-url += `&q=${encodeURIComponent(q)}`;
-}else{
-url += `&country=in`;
-}
-
-if(category){
-url += `&category=${category}`;
-}
-
-if(page){
-url += `&page=${page}`;
-}
-
-try{
-
-const response = await fetch(url);
-
-const data = await response.json();
-
-res.setHeader("Cache-Control","s-maxage=300");
-
-res.status(200).json(data);
-
-}catch(error){
-
-res.status(500).json({
-status:"error",
-message:error.message
-});
-
-}
-
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: error.message
+    });
+  }
 }
